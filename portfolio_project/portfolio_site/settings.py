e285cf7a07b595b20418292c3b0df38274dc80da
Mathew Rayan <jwmathew300@gmail.com>
2026-01-12 15:01:06 +0530

    update settings

--- portfolio_project/portfolio_site/settings.py
+++ portfolio_project/portfolio_site/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'replace-this-with-a-secure-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.auth',
